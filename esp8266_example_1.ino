c44808afe883df6cfb5aa9bf00ab29302e89c9ec
Somewheref <52316164+Somewheref@users.noreply.github.com>
2023-05-23 11:42:15 +0100

    Added working transmitter on ESP8266 & PC receiver

---
+++ esp8266_example_1.ino
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncWebSocket.h>
+
+const char* ssid = "EBB_AP";
+const char* password = "123456789";
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  if (type == WS_EVT_CONNECT) {
+    Serial.println("WebSocket client connected");
+  } else if (type == WS_EVT_DISCONNECT) {
+    Serial.println("WebSocket client disconnected");
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.softAP(ssid, password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  server.addHandler(&ws);
+  ws.onEvent(onWebSocketEvent);
+
+  server.begin();
+}
+
+void loop() {
+  ws.textAll(String(random(10)));
+  delay(1000);
+}
